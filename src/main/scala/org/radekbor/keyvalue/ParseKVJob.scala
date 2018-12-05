af6c0c44ba6a232aa442c9d1a33d6af00be98d04
radekbor <radoslaw.borowiecki@gmail.com>
2018-12-05 20:32:26 +0100

    Configure KeyValueTextInputFormat

--- src/main/scala/org/radekbor/keyvalue/ParseKVJob.scala
+++ src/main/scala/org/radekbor/keyvalue/ParseKVJob.scala
@@ -3,10 +3,23 @@ package org.radekbor.keyvalue
 import org.apache.hadoop.conf.{Configuration, Configured}
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io._
-import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapred.TextOutputFormat
 import org.apache.hadoop.mapreduce.lib.input.{FileInputFormat, KeyValueTextInputFormat}
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import org.apache.hadoop.mapreduce.lib.output.{FileOutputFormat, SequenceFileOutputFormat}
+import org.apache.hadoop.mapreduce.{Job, Mapper}
 import org.apache.hadoop.util.Tool
+import org.slf4j.LoggerFactory
+
+
+class ClubMapper extends Mapper[Text, Text, LongWritable, Text] {
+
+  private val log = LoggerFactory.getLogger(classOf[ClubMapper])
+
+  override def map(key: Text, value: Text, context: Mapper[Text, Text, LongWritable, Text]#Context): Unit = {
+    context.write(new LongWritable(key.toString.toInt), new Text(value))
+  }
+}
+
 
 class ParseKVJob extends Configured with Tool {
   override def run(args: Array[String]): Int = {
@@ -14,13 +27,13 @@ class ParseKVJob extends Configured with Tool {
 
     val job = Job.getInstance(configuration, "Parse Clubs")
     // TODO FIXME not working
-    configuration.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", ",")
     job.setJarByClass(this.getClass)
 
     job.setInputFormatClass(classOf[KeyValueTextInputFormat])
-    job.setOutputValueClass(classOf[Text])
-    job.setOutputKeyClass(classOf[Text])
+    job.setMapperClass(classOf[ClubMapper])
 
+    job.setOutputValueClass(classOf[Text])
+    job.setOutputKeyClass(classOf[LongWritable])
 
     FileInputFormat.addInputPath(job, new Path(args(0)))
     FileOutputFormat.setOutputPath(job, new Path(args(1)))
